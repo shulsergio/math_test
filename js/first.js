527e8bd7aa2db4446b72f1eb1a5abd6f41891cc5
Sergey Shulga <shul.sergio@gmail.com>
2024-06-04 18:15:41 +0300

    add-tests
    add tests js for console

---
+++ js/first.js
@@ -0,0 +1,23 @@
+console.log("--- .article");
+const article = document.querySelector(".article");
+console.log(article.innerHTML);
+
+console.log("--- .article .title");
+const title = document.querySelector(".article .title");
+console.log(title.innerHTML);
+
+console.log("--- .article .text");
+const text = document.querySelector(".article .text");
+console.log(text.innerHTML);
+
+console.log("--- .text2");
+const text2 = document.querySelector(".text");
+console.log(text.innerHTML);
+
+console.log("--- .article .link");
+const link = document.querySelector(".article .link");
+console.log(link.innerHTML);
+
+console.log("--- .text2");
+const link2 = document.querySelector(".link");
+console.log(link.innerHTML);
